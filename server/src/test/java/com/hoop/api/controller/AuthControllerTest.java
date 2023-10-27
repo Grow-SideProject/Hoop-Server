81ab0c3f0cf1e3bd2c4ef5a15f8909087d716e07
이지훈 <easy123@sk.com>
2023-10-27 14:27:41 +0900

    DOCS : API DOCS 포스트맨으로 변경

--- server/src/test/java/com/hoop/api/controller/AuthControllerTest.java
+++ server/src/test/java/com/hoop/api/controller/AuthControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
@@ -22,10 +21,6 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -79,7 +74,7 @@ class AuthControllerTest {
         when(kakaoService.getKakaoIdByToken("MOCKTOKEN")).thenReturn(12345L);
 
         // expected
-        mockMvc.perform(RestDocumentationRequestBuilders.post("/auth/signup/social")
+        mockMvc.perform(post("/auth/signup/social")
                         .content(objectMapper.writeValueAsString(signup))
                         .contentType(APPLICATION_JSON)
                         .accept(APPLICATION_JSON))
